fd4b6e10fa628637590411235ab3b1b972e7ccb0
MariaIsabel FierrosCastillo <hernandezfierrossonia@gmail.com>
2023-05-23 19:06:28 +0000

    entendiendo fibo

--- Lab11/fibo.py
+++ Lab11/fibo.py
@@ -1,14 +1,14 @@
 def recur_fibo(n):
-    if n<=1:
+    if n<=1: #se me hace muy inteligente, para elemento 1(más claro primera iteración, donde i vale 0)  de la serie, imprimir 0, para 2 elementos (más claro segunda iteración, donde i vale 1) de la serie imprimir 1
         return n
     else:
-        return(recur_fibo(n-1))+ recur_fibo(n-2)
+        return(recur_fibo(n-1))+ recur_fibo(n-2) #escencialmente aquí esta la instrucción, básicamente significa sumar el elemento anterior(último) con el anterior del anterior(penúltimo)
 
 nterms=13
 
 if nterms<=0:
-    print("Inrese un número entero positivo")
+    print("Por favor ingrese un número entero positivo.")
 else:
     print("Secuencia Fibonacci")
-    for i in range(nterms):
+    for i in range(nterms): #a caray, por que estamos en una iteración
         print(recur_fibo(i))
--- Pruebas_P_11/codigo5.py
+++ Pruebas_P_11/codigo5.py
@@ -13,7 +13,7 @@ def fibonacci_inverso(n):
         return [1, 0]
     else:
         fib = [0, 1]
-        for i in range(2, n):
+        for i in range(2, n):#era lo mismo que poner in range(n-2)
             fib.append(fib[-1] + fib[-2])
         return fib[::-1]
 
